4266db3ceeb601e135e1eb5ef18d61fa74c08461
jini <jinhui.ha1208@gmail.com>
2024-11-02 13:30:12 +0900

    🎨 수정

--- js/weather.js
+++ js/weather.js
@@ -1,25 +1,23 @@
-const API_KEY = "e60998a1a76410bc49c50a6a51f4925e";
+const API_KEY = 'e60998a1a76410bc49c50a6a51f4925e';
 
 // getCurrentPosition의 position 속성에서 값 불러오기
 function onGeoOk(position) {
-  // 위도 값 가져오기
-  const lat = position.coords.latitude;
-  // 경도 값 가져오기
-  const lng = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+    // 위도 값 가져오기
+    const lat = position.coords.latitude;
+    // 경도 값 가져오기
+    const lng = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
 
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      const weather = document.querySelector(".weather div:first-child");
-      const city = document.querySelector(".weather div:last-child");
-      city.textContent = data.name;
-      weather.textContent = `${data.weather[0].main} / ${(
-        Math.round(data.main.temp * 100) / 100
-      ).toFixed(1)}°C`;
-    });
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            const weather = document.querySelector('.weather div:first-child');
+            const city = document.querySelector('.weather div:last-child');
+            city.textContent = data.name;
+            weather.textContent = `${data.weather[0].main} / ${(Math.round(data.main.temp * 100) / 100).toFixed(1)}°C`;
+        });
 }
 function onGeoError() {
-  alert("더 좋은 페이지를 위해 설정에서 위치 정보를 허용해주세요 :)");
+    alert('더 좋은 페이지를 위해 설정에서 위치 정보를 허용해주세요 :)');
 }
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
