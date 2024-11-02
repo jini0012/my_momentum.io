06c59f58e1d7dda1e8e40eaa0747fe52922a656b
jini <jinhui.ha1208@gmail.com>
2024-11-02 13:27:04 +0900

    🎨 모바일 화면 일부 적용

--- js/background.js
+++ js/background.js
@@ -1,41 +1,39 @@
 const images = [
-  "0.jpg",
-  "1.jpg",
-  "2.png",
-  "3.jpg",
-  "4.jpg",
-  "5.png",
-  "6.png",
-  "7.jpg",
-  "8.png",
-  "9.png",
-  "10.jpg",
-  "11.jpg",
-  "12.jpg",
-  "13.png",
-  "14.png",
-  "15.png",
+    '0.jpg',
+    '1.jpg',
+    '2.png',
+    '3.jpg',
+    '4.jpg',
+    '5.png',
+    '6.png',
+    '7.jpg',
+    '8.png',
+    '9.png',
+    '10.jpg',
+    '11.jpg',
+    '12.jpg',
+    '13.png',
+    '14.png',
+    '15.png',
 ];
 
 const backgroundImage = images[Math.floor(Math.random() * images.length)];
 
-// const image = document.createElement("img");
-// image.src = `images/${backgroundImage}`;
-// document.body.appendChild(image);
-document.body.style.backgroundImage = `url(./images/${backgroundImage})`;
+const body = document.body;
+const btn = document.querySelector('.changeBackground');
+
+body.style.backgroundImage = `url(./images/${backgroundImage})`;
 
 if (
-  backgroundImage === "8.png" ||
-  backgroundImage === "5.png" ||
-  backgroundImage === "11.jpg" ||
-  backgroundImage === "14.png" ||
-  backgroundImage === "15.png"
+    backgroundImage === '8.png' ||
+    backgroundImage === '5.png' ||
+    backgroundImage === '11.jpg' ||
+    backgroundImage === '14.png' ||
+    backgroundImage === '15.png'
 ) {
-  document.body.style.color = "black";
+    body.style.color = 'black';
 }
 
-const btn = document.querySelector(".changeBackground");
-
-btn.addEventListener("click", () => {
-  location.reload();
+btn.addEventListener('click', () => {
+    location.reload();
 });
--- js/weather.js
+++ js/weather.js
@@ -8,11 +8,9 @@ function onGeoOk(position) {
   const lng = position.coords.longitude;
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
 
-  console.log(url);
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
       const weather = document.querySelector(".weather div:first-child");
       const city = document.querySelector(".weather div:last-child");
       city.textContent = data.name;
